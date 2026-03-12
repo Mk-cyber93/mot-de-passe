ea3e69c5c685a937fdece289fad7912b979f217c
Mk-cyber93 <ilyesmak655@gmail.com>
2026-03-12 14:37:13 +0100

    mot de passe

---
+++ script.py
@@ -0,0 +1,24 @@
+mot_de_passe = "python"
+Entre = None
+essais = 3
+changé = None
+while essais > 0:
+
+       Entre = input("Entre le mot de passe : ")
+
+       if Entre ==mot_de_passe:
+            print("Accès autorisé")
+            changé = input("Veux-tu changer le mot de passe ? oui/non : ")
+       if changé == "oui":
+            mot_de_passe = input("Nouveau mot de passe : ")
+            print("Mot de passe changé !")
+            break 
+       if changé == "non": 
+          break    
+       elif essais ==0:
+            print("Accès bloqué")
+       else :
+            essais = essais - 1
+            print("code incorrect")
+            print("il reste", essais ,"essai")
+print("fin du programme")
